d55c4906943ff474177395aa998a970b9996be66
Jay <xuehongjiej@163.com>
2020-06-21 16:15:36 +0800

    update tictactoe

--- week10/homework/tictactoe/tictactoe.js
+++ week10/homework/tictactoe/tictactoe.js
@@ -1,176 +1,175 @@
-let pattern = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
-let value = ['', '⭕️', '❌'];
-let alertText = ['', 'O is winner!', 'X is winner!'];
-let willText = ['', 'O will win!', 'X will win!'];
-let key = 1;
-let length = value.length;
-let isOver = false;
+class TicTacToe {
+  constructor() {
+    // 每个步骤的值
+    let stepMap = {
+      0: '',
+      1: '⭕️',
+      2: '❌',
+    };
 
-// 绘制棋谱
-function show() {
-  let board = document.getElementById('board');
-  board.innerHTML = '';
-  for (let y = 0; y < length; y++) {
-    for (let x = 0; x < length; x++) {
-      let cell = document.createElement('div');
-      cell.classList.add('cell');
-      cell.innerHTML = value[pattern[y][x]];
-      board.appendChild(cell);
-      cell.addEventListener('click', () => {
-        if (isOver) return;
-        move(x, y);
-      });
+    this.pattern = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+
+    this.stepValue = 1; // 当前步骤的值
+    this.stepMap = stepMap; // 步骤映射表
+    this.len = Object.values(stepMap).length; // 所有可用步骤的长度
+
+    this.alertText = ['', 'O is winner!', 'X is winner!'];
+    this.willText = ['', 'O will win!', 'X will win!'];
+    this.isOver = false;
+  }
+  // 渲染棋谱
+  show() {
+    let len = this.len;
+    let pattern = this.pattern;
+    let board = document.getElementById('board');
+    board.innerHTML = '';
+    for (let y = 0; y < len; y++) {
+      for (let x = 0; x < len; x++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.innerHTML = this.stepMap[pattern[y][x]];
+        board.appendChild(cell);
+        cell.addEventListener('click', () => {
+          if (this.isOver) return;
+          this.move(x, y);
+        });
+      }
+      board.appendChild(document.createElement('br'));
     }
-    board.appendChild(document.createElement('br'));
   }
-}
-// 点击落子
-function move(x, y) {
-  if (pattern[y][x]) return;
-  pattern[y][x] = key;
-  if (check(pattern, key)) {
-    alert(alertText[key]);
-    isOver = true;
-  }
-  key = length - key;
-  show();
-  computerMove();
-  console.log(bestChoice(pattern, key).point);
-  if (willWin(pattern, key)) console.log(willText[key]);
-}
+  // 落子
+  move(x, y) {
+    if (this.pattern[y][x]) {
+      return;
+    }
+    this.pattern[y][x] = this.stepValue;
+    if (this.check(this.pattern, this.stepValue)) {
+      alert(this.alertText[this.stepValue]);
+      this.isOver = true;
+    }
+    this.stepValue = this.len - this.stepValue;
+    this.show();
+    this.computerMove();
 
-// 机器落子
-function computerMove() {
-  let choice = bestChoice(pattern, key);
-  if (choice.point) {
-    pattern[choice.point[1]][choice.point[0]] = key;
+    console.log(this.bestChoice(this.pattern, this.stepValue).point);
+    if (this.willWin(this.pattern, this.stepValue)) {
+      console.log(this.willText[this.stepValue]);
+    }
   }
-  if (check(pattern, key)) {
-    alert(alertText[key]);
-    isOver = true;
+  // 机器落子
+  computerMove() {
+    let choice = this.bestChoice(this.pattern, this.stepValue);
+    if (choice.point) {
+      this.pattern[choice.point[1]][choice.point[0]] = this.stepValue;
+    }
+    if (this.check(this.pattern, this.stepValue)) {
+      alert(this.alertText[this.stepValue]);
+      this.isOver = true;
+    }
+    this.stepValue = this.len - this.stepValue;
+    this.show();
   }
-  key = length - key;
-  show();
-}
-// 检查是否胜利
-function check(pattern, key) {
-  // 横向检查
-  for (let y = 0; y < length; y++) {
-    let win = true;
-    for (let x = 0; x < length; x++) {
-      if (pattern[y][x] !== key) {
-        win = false;
-        break;
+  // 检查是否胜利
+  check(pattern, stepValue) {
+    let len = this.len;
+    // 横向检查
+    for (let y = 0; y < len; y++) {
+      let win = true;
+      for (let x = 0; x < len; x++) {
+        if (pattern[y][x] !== stepValue) {
+          win = false;
+          break;
+        }
+      }
+      if (win) return win;
+    }
+    // 纵向检查
+    for (let y = 0; y < len; y++) {
+      let win = true;
+      for (let x = 0; x < len; x++) {
+        if (pattern[x][y] !== stepValue) {
+          win = false;
+          break;
+        }
+      }
+      if (win) return win;
+    }
+    // 左上至右下，对角检查
+    {
+      let win = true;
+      for (let i = 0; i < len; i++) {
+        if (pattern[i][i] !== stepValue) {
+          win = false;
+          break;
+        }
+      }
+      if (win) return win;
+    }
+    // 左下至右上，对角检查
+    {
+      let win = true;
+      for (let i = 0; i < len; i++) {
+        if (pattern[i][len - i] !== stepValue) {
+          win = false;
+          break;
+        }
+      }
+      if (win) return win;
+    }
+    return false;
+  }
+  // 拷贝对象
+  clone(pattern) {
+    return JSON.parse(JSON.stringify(pattern));
+  }
+  // 判断是否即将胜利
+  willWin(pattern, stepValue) {
+    let len = this.len;
+    for (let y = 0; y < len; y++) {
+      for (let x = 0; x < len; x++) {
+        if (pattern[y][x]) continue;
+        let tmp = this.clone(pattern);
+        tmp[y][x] = stepValue;
+        if (this.check(tmp, stepValue)) return [x, y];
       }
     }
-    if (win) return win;
+    return null;
   }
-  // 纵向检查
-  for (let y = 0; y < length; y++) {
-    let win = true;
-    for (let x = 0; x < length; x++) {
-      if (pattern[x][y] !== key) {
-        win = false;
-        break;
+  // 判断最佳落子点
+  bestChoice(pattern, stepValue) {
+    let point = this.willWin(pattern, stepValue);
+    if (point) {
+      return {
+        point,
+        result: 1,
+      };
+    }
+    let result = -1;
+    let len = this.len;
+    for (let y = 0; y < len; y++) {
+      for (let x = 0; x < len; x++) {
+        if (pattern[y][x]) continue;
+        let tmp = this.clone(pattern);
+        tmp[y][x] = stepValue;
+        let opp = this.bestChoice(tmp, len - stepValue);
+        if (-opp.result >= result) {
+          point = [x, y];
+          result = -opp.result;
+        }
       }
     }
-    if (win) return win;
-  }
-  // 左上至右下，对角检查
-  {
-    let win = true;
-    for (let i = 0; i < length; i++) {
-      if (pattern[i][i] !== key) {
-        win = false;
-        break;
-      }
-    }
-    if (win) return win;
-  }
-  // 左下至右上，对角检查
-  {
-    let win = true;
-    for (let i = 0; i < length; i++) {
-      if (pattern[i][2 - i] !== key) {
-        win = false;
-        break;
-      }
-    }
-    if (win) return win;
-  }
-  return false;
-}
 
-function clone(pattern) {
-  return JSON.parse(JSON.stringify(pattern));
-}
-function willWin(pattern, key) {
-  for (let y = 0; y < length; y++) {
-    for (let x = 0; x < length; x++) {
-      if (pattern[y][x]) continue;
-      let tmp = clone(pattern);
-      tmp[y][x] = key;
-      if (check(tmp, key)) return [x, y];
-    }
-  }
-  return null;
-}
-
-// 棋谱功能
-// let openings = new Map();
-
-// openings.set([
-//     [0, 0, 0],
-//     [0, 0, 0],
-//     [0, 0, 0]
-// ].toString(), {
-//     point: [1, 1],
-//     result: 0
-// })
-// 最佳落子点，并返回结果
-function bestChoice(pattern, key) {
-  // 如果棋谱里有过记录，直接使用棋谱
-  // if (openings.has(pattern.toString())) {
-  //     return openings.get(pattern.toString());
-  // }
-  let point = willWin(pattern, key);
-  if (point) {
-    // openings.set(pattern.toString(), {
-    //     point,
-    //     result: 1
-    // })
     return {
       point,
-      result: 1,
+      result: point ? result : 0,
     };
   }
-  let result = -1;
-  for (let y = 0; y < length; y++) {
-    for (let x = 0; x < length; x++) {
-      if (pattern[y][x]) continue;
-      let tmp = clone(pattern);
-      tmp[y][x] = key;
-      let opp = bestChoice(tmp, length - key);
-      // openings.set(tmp.toString(), {
-      //     point: [x, y],
-      //     result: - opp.result
-      // })
-      // TODO: 这里的判断是什么意思
-      if (-opp.result >= result) {
-        point = [x, y];
-        result = -opp.result;
-      }
-    }
-  }
-
-  return {
-    point,
-    result: point ? result : 0,
-  };
 }
 
-show();
+let game = new TicTacToe();
+
+game.show();
